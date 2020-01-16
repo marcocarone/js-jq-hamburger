1a561de3bb5728b366c2853e51347d92ca63d438
Marco <marco.carone@libero.it>
2020-01-16 15:02:36 +0100

    fine

---
+++ main.js
@@ -0,0 +1,18 @@
+$( "i.fas.fa-bars" ).click(function() {
+  $(".hamburger-menu").fadeIn(500);
+});
+
+$("i.fas.fa-times").click( function() {
+  $(".hamburger-menu").fadeOut("fast");
+});
+
+
+// ALTRO MEDOTO
+//
+// $( "i.fas.fa-bars" ).click(function() {
+//   $(".hamburger-menu").show(500);
+// });
+//
+// $("i.fas.fa-times").click( function() {
+//   $(".hamburger-menu").hide("fast");
+// });
